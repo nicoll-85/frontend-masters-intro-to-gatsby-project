aa429a6fc2f10dd34f500fb5c358195d3c7e64f4
Nadia Aguilar <nadiaaguilar@Nadias-MacBook-Pro.local>
2021-08-13 14:28:54 -0600

    Add header

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl:'https://www.hellofellas.tld',
+        title: 'Gatsby fellas',
+        description: 'Intro to Gatsby fellas',
+        image:'https://images.pexels.com/photos/1988693/pexels-photo-1988693.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+    }
+};
