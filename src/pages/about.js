c6de4a380e0b58af0a6ac5fc901575bd919f1537
Nadia Aguilar <nadiaaguilar@Nadias-MacBook-Pro.local>
2021-08-12 22:13:33 -0600

    Add links between

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import *as React from 'react';
+import {Link} from 'gatsby';
 
 export default function AboutPage() {
     return(
         <main>
             <h1>About this fellas</h1>
+            <Link to="/">About page </Link>
         </main>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import *as React from 'react';
+import {Link} from 'gatsby';
 
 export default function IndexPage() {
     return(
         <main>
             <h1>Hello Fellas!</h1>
+            <Link to="/about">Back to Home </Link>
         </main>
     );
 }
